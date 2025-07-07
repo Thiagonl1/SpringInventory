6553c1c606a1d1ccf31c4e2411c544e5d01bf905
Lapis <thiagoll284@gmail.com>
2025-07-07 01:16:08 -0300

    Feat: Added "User" class

---
+++ src/main/java/com/thiago/inventario/model/User.java
@@ -0,0 +1,13 @@
+package com.thiago.inventario.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class User {
+    @Id
+    private int id;
+    private String username;
+    private String password;
+    private String role;
+}
